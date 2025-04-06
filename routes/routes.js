a07fd907f722445166b65005ba8242bbdf2ab550
jhon <jhonmarcosaraujo@yahoo.com>
2025-04-06 17:08:39 -0400

    retries http request

--- routes/routes.js
+++ routes/routes.js
@@ -4,16 +4,36 @@ import { Router } from "express";
 const router = Router()
 
 
+async function fetchWithRetry(url, options, retries = 3) {
+  
+  for (let i = 0; i < retries; i++) {
+      try {
+
+          return await fetch(url, options);
+      } catch (error) {
+          console.error(`Attemp ${i + 1} failed:`, error);
+
+          if (i === retries - 1) {
+              throw new Error("Can not fetch ");
+          }
+      }
+  }
+}
+
 
 router.get('/:continent/:country', async (req, res) => {
-
+  
 
   const continent = req.params.continent;
   const country = req.params.country;
   const location = `${continent}/${country}`;
 
+  const controller = new AbortController();
+  const timeout = setTimeout(() => controller.abort(), 5000);
+
   try {
-      const response = await fetch(`https://timeapi.io/api/Time/current/zone?timeZone=${location}`);
+      const response = await fetchWithRetry(`https://timeapi.io/api/Time/current/zone?timeZone=${location}`);
+      clearTimeout(timeout);
       
       if (!response.ok) {
         throw new Error(`Error fetching data: ${response.statusText}`);
