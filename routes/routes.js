39f30e5172a9b4b8e2315fe2bd17f01945260353
jhon <jhonmarcosaraujo@yahoo.com>
2025-04-17 21:38:37 -0400

    the __ validation

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,12 @@ import { Router } from "express";
 const router = Router()
 
 
-async function fetchWithRetry(url, options, retries = 3) {
+function delete__(Name_){
+  
+  return Name_.replace("_", " ")  // delete _ and replace _ with " "
+}
+
+async function fetchWithRetry(url, options, retries = 3) {  //retry the http request if  the api thow an error
   
   for (let i = 0; i < retries; i++) {
       try {
@@ -33,17 +38,22 @@ router.get('/:continent/:country', async (req, res) => {
   const timeout = setTimeout(() => controller.abort(), 5000);
 
   try {
+
       const response = await fetchWithRetry(`https://timeapi.io/api/Time/current/zone?timeZone=${location}`);
       clearTimeout(timeout);
-      
+
       if (!response.ok) {
         throw new Error(`Error fetching data: ${response.statusText}`);
       }
       
+
+
       const data = await response.json(); 
 
+      let country__ = delete__(country)  
 
-      res.render("index", { Country: country, date: data["date"], time: data["time"] });
+      res.render("index", { Country: country__, date: data["date"], time: data["time"] });
+
   } catch (error) {
       console.error(error);
       res.status(500).send("Can not fetch the time");
@@ -52,7 +62,7 @@ router.get('/:continent/:country', async (req, res) => {
 
 
 router.get('/',(req,res) => {
-
+// redirect to / if not found
   res.redirect("/America/Santo_Domingo");
 
 });
