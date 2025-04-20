beba1281bc652841f563add40a712f4887e39527
jhon <jhonmarcosaraujo@yahoo.com>
2025-04-20 17:33:17 -0400

    oceania continent added and button for dark mode deprecated

--- public/JS/index.js
+++ public/JS/index.js
@@ -1,5 +1,4 @@
 
-
 function change_styles() {  /// change styles
 
 
@@ -8,7 +7,6 @@ function change_styles() {  /// change styles
   const h1 = document.getElementById('Current');
   const timeHeading = document.getElementById('Time');
   const dateHeading = document.getElementById('Date');
-  const colorButton = document.getElementById('l_d_button');
 
   const current_background = window.getComputedStyle(body).backgroundColor;
   let isDarkMode = (current_background !== 'rgb(255, 255, 255)' && current_background !== 'white');
@@ -18,7 +16,6 @@ function change_styles() {  /// change styles
     backgroundColor: isDarkMode ? 'white' : 'black',
     textColor: isDarkMode ? 'black' : 'white',
     buttonText: isDarkMode ? 'Dark' : 'light',
-    buttonClass: isDarkMode ? 'btn btn-dark' : 'btn btn-light',
         
       // this array save the theme: light or dark.....
   };
@@ -29,8 +26,6 @@ function change_styles() {  /// change styles
   h1.style.color = styles.textColor;
   timeHeading.style.color = styles.textColor;
   dateHeading.style.color = styles.textColor;
-  colorButton.textContent = styles.buttonText;
-  colorButton.className = styles.buttonClass;
   localStorage.setItem('themeStyles', JSON.stringify(styles));  // set the item with local storage for using in loadSaveStyles func...
 
 }
@@ -52,16 +47,14 @@ function loadSavedStyles() {   // load the saved styles
     const dateHeading = document.getElementById('Date');
 
 
-    const colorButton = document.getElementById('l_d_button');
-    const _navbar_ = document.getElementById('navbar_');
+    
 
     body.style.backgroundColor = savedStyles.backgroundColor;
     h1.style.color = savedStyles.textColor;
     timeHeading.style.color = savedStyles.textColor;
     dateHeading.style.color = savedStyles.textColor;
 
-    colorButton.textContent = savedStyles.buttonText;
-    colorButton.className = savedStyles.buttonClass;
+  
   }
 }
 
